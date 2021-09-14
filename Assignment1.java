3b2f2abc2335ac6875acf3946c329eea463f497c
Colin-Bradshaw <90667786+Colin-Bradshaw@users.noreply.github.com>
2021-09-14 02:02:05 -0400

    Update Assignment1.java
    Cleaned up print line statements to improve readability
    Made the three parameter printChar independent of the two parameter version

--- Assignment1.java
+++ Assignment1.java
@@ -12,36 +12,28 @@ public class Assignment1{
 		System.out.println("1)");
 		for(int i = 1; i < 5; i++){
 			Fo.printChar('*', i);
-			System.out.println();
 		}
 		Fo.printChar('.', 9);
-		System.out.println();
 		
 		// pattern 2
 		System.out.println("2)");
 		Fo.printChar('.', 9);
-		System.out.println();
 		for(int i = 4; i > 0; i--){
 			Fo.printChar('*', i);
-			System.out.println();
 		}
 		
 		//pattern 3
 		System.out.println("3)");
 		for(int i = 1; i < 9; i+=2){
 			Fo.printChar('*', i, 11);
-			System.out.println();
 		}
 		Fo.printChar('.', 11);
-		System.out.println();
 		
 		// pattern 4
 		System.out.println("4)");
 		Fo.printChar('.', 11);
-		System.out.println();
 		for(int i = 7; i > 0; i-=2){
 			Fo.printChar('*', i, 11);
-			System.out.println();
 		}
 	}
 	
@@ -50,19 +42,25 @@ public class Assignment1{
 		if(n > 0){
 			System.out.print(x);
 			printChar(x, n-1);
+		} else {
+			System.out.println();
 		}
 	}
 	
 	// allows for left and right padding totalling (width - n) / 2
-	// ideally, width - n  should be an even number
+	// ideally, (width - n)  should be an even number
 	private void printChar(char x, int n, int width){
 		if(n > 0) {
-			printChar(' ', ((width - n) / 2));
-			printChar(x, n);
-			printChar(' ', ((width - n) / 2));
+			for(int i =0; i < (width - n) / 2; i++){
+				System.out.print(' ');
+			}
+			for(int i = 0; i < n; i++){
+				System.out.print(x);
+			}
+			for(int i =0; i < (width - n) / 2; i++){
+				System.out.print(' ');
+			}
+			System.out.println();
 		}
 	}
-	
 }
-
-
